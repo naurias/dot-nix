164db5585d9d832e9a7f5b22eb48ff2cfd1a6db1
naurias <dev.earmark338@passinbox.com>
2026-07-05 14:10:23 +0500

    added hyprland

---
+++ hypr/extra.lua
@@ -0,0 +1,314 @@
+
+-- extraConfig
+            -- Curves
+hl.curve("expressiveFastSpatial", {
+    type = "bezier",
+    points = {{0.42, 1.67}, {0.21, 0.90}}
+})
+hl.curve("expressiveSlowSpatial", {
+    type = "bezier",
+    points = {{0.39, 1.29}, {0.35, 0.98}}
+})
+hl.curve("expressiveDefaultSpatial", {
+    type = "bezier",
+    points = {{0.38, 1.21}, {0.22, 1.00}}
+})
+hl.curve("emphasizedDecel", {
+    type = "bezier",
+    points = {{0.05, 0.7}, {0.1, 1}}
+})
+hl.curve("emphasizedAccel", {
+    type = "bezier",
+    points = {{0.3, 0}, {0.8, 0.15}}
+})
+hl.curve("standardDecel", {
+    type = "bezier",
+    points = {{0, 0}, {0, 1}}
+})
+hl.curve("menu_decel", {
+    type = "bezier",
+    points = {{0.1, 1}, {0, 1}}
+})
+hl.curve("menu_accel", {
+    type = "bezier",
+    points = {{0.52, 0.03}, {0.72, 0.08}}
+})
+hl.curve("stall", {
+    type = "bezier",
+    points = {{1, -0.1}, {0.7, 0.85}}
+})
+-- Configs
+-- windows
+hl.animation({
+    leaf = "windowsIn",
+    enabled = true,
+    speed = 3,
+    bezier = "emphasizedDecel",
+    style = "popin 80%"
+})
+hl.animation({
+    leaf = "fadeIn",
+    enabled = true,
+    speed = 3,
+    bezier = "emphasizedDecel"
+})
+hl.animation({
+    leaf = "windowsOut",
+    enabled = true,
+    speed = 2,
+    bezier = "emphasizedDecel",
+    style = "popin 90%"
+})
+hl.animation({
+    leaf = "fadeOut",
+    enabled = true,
+    speed = 2,
+    bezier = "emphasizedDecel"
+})
+hl.animation({
+    leaf = "windowsMove",
+    enabled = true,
+    speed = 3,
+    bezier = "emphasizedDecel",
+    style = "slide"
+})
+hl.animation({
+    leaf = "border",
+    enabled = true,
+    speed = 10,
+    bezier = "emphasizedDecel"
+})
+
+-- layers
+hl.animation({
+    leaf = "layersIn",
+    enabled = true,
+    speed = 2.7,
+    bezier = "emphasizedDecel",
+    style = "popin 93%"
+})
+hl.animation({
+    leaf = "layersOut",
+    enabled = true,
+    speed = 2.4,
+    bezier = "menu_accel",
+    style = "popin 94%"
+})
+-- fade
+hl.animation({
+    leaf = "fadeLayersIn",
+    enabled = true,
+    speed = 0.5,
+    bezier = "menu_decel"
+})
+hl.animation({
+    leaf = "fadeLayersOut",
+    enabled = true,
+    speed = 2.7,
+    bezier = "stall"
+})
+-- workspaces
+hl.animation({
+    leaf = "workspaces",
+    enabled = true,
+    speed = 7,
+    bezier = "menu_decel",
+    style = "slide"
+})
+-- specialWorkspace
+hl.animation({
+    leaf = "specialWorkspaceIn",
+    enabled = true,
+    speed = 2.8,
+    bezier = "emphasizedDecel",
+    style = "slidevert"
+})
+hl.animation({
+    leaf = "specialWorkspaceOut",
+    enabled = true,
+    speed = 1.2,
+    bezier = "emphasizedAccel",
+    style = "slidevert"
+})
+-- zoom
+hl.animation({
+    leaf = "zoomFactor",
+    enabled = true,
+    speed = 3,
+    bezier = "standardDecel"
+})
+
+
+
+--##! Window
+--# Focusing
+hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true, description = "Window: Move" })
+hl.bind("SUPER + mouse:274", hl.dsp.window.drag(), { mouse = true })
+hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true, description = "Window: Resize" })
+--#/# bind = SUPER + ←/↑/→/↓,, -- Focus in direction
+for i = 1, 4 do
+    local arrowkey = { "Left", "Right", "Up", "Down" }
+    local focusdir = { "l", "r", "u", "d" }
+    hl.bind("SUPER + " .. arrowkey[i], hl.dsp.focus({ direction = focusdir[i] }),
+        { description = "Window: Focus " .. arrowkey[i] })
+end
+--#/# bind = SUPER + SHIFT, ←/↑/→/↓,, -- Move in direction
+for i = 1, 4 do
+    local arrowkey = { "Left", "Right", "Up", "Down" }
+    local focusdir = { "l", "r", "u", "d" }
+    hl.bind("SUPER + SHIFT + " .. arrowkey[i], hl.dsp.window.move({ direction = focusdir[i] }),
+        { description = "Window: Move " .. arrowkey[i] })
+end
+
+
+for i = 1, 4 do
+    local arrowkey = { "Left", "Right", "Up", "Down" }
+    local focusdir = { "l", "r", "u", "d" }
+    hl.bind("ALT + SHIFT + " .. arrowkey[i], hl.dsp.window.move({ direction = focusdir[i], group_aware = true }),
+        { description = "Window: Move " .. arrowkey[i] })
+end
+
+
+
+--# Positioning mode
+hl.bind("SUPER + S", hl.dsp.window.float({ action = "toggle" }), { description = "Window: Float/Tile" })
+hl.bind("SUPER + M", hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }),
+    { description = "Window: Maximize" })
+hl.bind("SUPER + F", hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" }),
+    { description = "Window: Fullscreen" })
+hl.bind("SUPER + SHIFT + F", hl.dsp.window.fullscreen_state({ internal = 0, client = 2, action = "toggle" }),
+    { description = "Window: Fullscreen spoof" })
+hl.bind("SUPER + P", hl.dsp.window.pin(), { description = "Window: Pin" })
+
+
+
+for i = 1, 10 do
+	local key = i % 10
+	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+end
+
+-- Switch to a submap called `resize`.
+hl.bind("SUPER + R", hl.dsp.submap("resize"))
+hl.bind("SUPER + ALT + R", hl.dsp.submap ("move"))
+-- Start a submap called "resize".
+hl.define_submap("resize", function()
+
+    -- Set repeating binds for resizing the active window.
+    hl.bind("right", hl.dsp.window.resize({ x = 10, y = 0, relative = true}), { repeating = true })
+    hl.bind("left", hl.dsp.window.resize({ x = -10, y = 0, relative = true}), { repeating = true })
+    hl.bind("up", hl.dsp.window.resize({ x = 0, y = 10, relative = true}), { repeating = true })
+    hl.bind("down", hl.dsp.window.resize({ x = 0, y = -10, relative = true}), { repeating = true })
+
+    -- Use `reset` to go back to the global submap
+    hl.bind("escape", hl.dsp.submap("reset"))
+
+end)
+
+hl.define_submap("move", function()
+    hl.bind("right", hl.dsp.window.move({ x = 10, y = 0, relative = true}), { repeating = true})
+    hl.bind("left", hl.dsp.window.move({ x = -10, y = 0, relative = true}), { repeating = true})
+    hl.bind("up", hl.dsp.window.move({ x = 0, y = -10, relative = true}), { repeating = true})
+    hl.bind("down", hl.dsp.window.move({ x = 0, y = 10, relative = true}), { repeating = true})
+
+
+    hl.bind("escape", hl.dsp.submap("reset"))
+end)
+
+hl.bind("SUPER + RETURN", (hl.dsp.exec_cmd("kitty")))
+hl.bind("SUPER + D", (hl.dsp.exec_cmd("noctalia msg panel-toggle launcher")))
+hl.bind("SUPER + W", (hl.dsp.window.close()))
+hl.bind("SUPER + SHIFT + W", (hl.dsp.exec_cmd("hyprshutdown")))
+hl.bind("SUPER + Tab", (hl.dsp.window.cycle_next()))
+
+hl.bind("SUPER + SHIFT + V", hl.dsp.window.move({ workspace = "special:magic" }))
+-- To see the hidden window and workspace you can use: 
+hl.bind("SUPER + V", hl.dsp.workspace.toggle_special("magic"))
+
+hl.bind("SUPER + G", hl.dsp.group.toggle())
+hl.bind("SUPER + period", hl.dsp.group.next())
+hl.bind("SUPER + comma", hl.dsp.group.prev())
+hl.bind("SUPER + BracketLeft", hl.dsp.layout("swapcol l"))
+hl.bind("SUPER + BracketRight", hl.dsp.layout("swapcol r"))
+-- Bind SUPER + P to toggle a pinned window
+hl.bind("SUPER + T", function()
+    local w = hl.get_active_window()
+    if w ~= nil then
+        -- Windows must be floating to be pinned
+        hl.dispatch(hl.dsp.window.float({ action = "set" })) 
+        hl.dispatch(hl.dsp.window.pin({ action = "toggle" }))
+    end
+end)
+
+
+
+
+
+hl.bind("SUPER + B", function ()
+    local layouts     = { "scrolling", "master" }
+    local workspace   = hl.get_active_workspace()
+	if hl.get_active_special_workspace() then
+		workspace = hl.get_active_special_workspace()
+	end
+
+    local next_layout = "dwindle"
+
+    if not workspace then
+        return
+    end
+
+    for i = 1, #layouts do
+        if layouts[i] == workspace.tiled_layout then
+            local next_layout_idx = (i % #layouts) + 1
+            next_layout = layouts[next_layout_idx]
+            break
+        end
+    end
+
+	if workspace.special then
+		hl.workspace_rule({ workspace = tostring(workspace.name), layout = next_layout })
+	else
+		hl.workspace_rule({ workspace = tostring(workspace.id), layout = next_layout })
+	end
+end)
+
+
+
+-- gamemode
+hl.bind("SUPER + F1", function ()
+    local game_mode = (hl.get_config("animations.enabled") == false)
+
+    if game_mode then
+        hl.exec_cmd("hyprctl reload")
+        return
+    end
+    
+    hl.config({
+        general = {
+            gaps_in = 0, gaps_out = 0, -- Disable gaps  
+            border_size = 0,
+        },
+
+        animations = {
+            enabled = false, -- Disable animations
+        },
+        
+        -- Disable blur, shadow and window rounding
+        decoration = {
+            shadow = { enabled = false },
+            blur = { enabled = false },
+            rounding = 0,
+        }
+    })
+end)
+
+
+
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd( "brightnessctl s 10%+"),
+    { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl s 10%-"),
+    { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ -l 1.5"),
+    { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-"),
+    { locked = true, repeating = true })
