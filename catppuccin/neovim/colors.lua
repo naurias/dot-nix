7ecf95dfd2af2604f810af6a8de24287959e1d6a
naurias <dev.earmark338@passinbox.com>
2026-09-14 21:50:40 +0500

    added neovim colors

---
+++ catppuccin/neovim/colors.lua
@@ -0,0 +1,14 @@
+return {
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "catppuccin/nvim" },
+	{ "rose-pine/neovim" },
+	-- Configure LazyVim 16:29:20o load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha",
+		},
+	},
+}
